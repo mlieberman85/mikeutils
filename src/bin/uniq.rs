d7cc96dd96e2e9032eaa5107419a8baa1d553711
Michael Lieberman <mlieberman85@gmail.com>
2016-09-17 10:47:08 -0400

    Fix loop for uniq
    It was doing branching on every loop to see if the line was Some or
    None whereas it only needs to check for the error once.

--- src/bin/uniq.rs
+++ src/bin/uniq.rs
@@ -1,20 +1,25 @@
 use std::io;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let stdin = io::stdin();
 
-    let mut previous = None;
+    let mut lines = stdin.lock().lines();
+    let first = lines.next();
+    
+    let mut previous = match first.unwrap() { 
+        Ok(some_string) => some_string,
+        Err(e) => process::exit(1)
+    };
 
-    for line in stdin.lock().lines() {
+    println!("{}", previous);
+
+    for line in lines {
         let cursor = line.unwrap();
-        if let Some(x) = previous {
-            if cursor != x {
-                println!("{}", cursor);
-            }
-        } else {
+        if cursor != previous {
             println!("{}", cursor);
         }
-        previous = Some(cursor);
+        previous = cursor;
     }
 }
